9152fb62a79356fb333c4d91bde05cdcfcaa5e60
Jessica Agorye <agoryejessica1@gmail.com>
2024-09-06 21:49:48 +0100

    removed margin from font awesome icon

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <>
       <div className="text-4xl ml-10 mt-6 flex gap-1">
-        <FontAwesomeIcon className="mt-1" icon={faMagnifyingGlassLocation} />
+        <FontAwesomeIcon icon={faMagnifyingGlassLocation} />
         <p>Fynderr</p>
       </div>
     </>
