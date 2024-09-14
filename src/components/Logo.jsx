221dde6559b9af184a57011fdf8351629d50a4d6
Agorye Jessica <agoryejessica1@gmail.com>
2024-09-14 15:17:15 +0100

    added fontawesome

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,11 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faMagnifyingGlassLocation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlassLocation } from "@fortawesome/free-solid-svg-icons";
 
 const Logo = () => {
   return (
     <>
       <div className="text-4xl ml-10 mt-6 flex gap-1">
-        {/* <FontAwesomeIcon icon={faMagnifyingGlassLocation} /> */}
+        <FontAwesomeIcon icon={faMagnifyingGlassLocation} />
         <p>Fynderr</p>
       </div>
     </>
